6591e016b216cb9bde7aab36b58308f86a052445
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-08-14 22:08:15 -0400

    Continue Problem 2 - Append nodes to result head node

--- C++/Problem2_AddTwoNumbers/AddTwoNumbers.cpp
+++ C++/Problem2_AddTwoNumbers/AddTwoNumbers.cpp
@@ -33,21 +33,52 @@ struct ListNode {
 class Solution {
 public:
 	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-		ListNode* resultList = &ListNode(NULL);
-		int sum;
-		int carryDigit;
+		ListNode* resultList = new ListNode(NULL);
+		ListNode* lastNode = new ListNode(NULL);
+		ListNode* currNode = new ListNode(NULL);
+		ListNode* nextNode = new ListNode(NULL);
+		ListNode* zeroNode = new ListNode(0);
+
+		int sum = 0;
+		int carryDigit = 0;
+
 		ListNode* list1 = l1;
 		ListNode* list2 = l2;
-		
-		while(list1 && list2) {
-			std::cout << list1->val;
-			//sum = l1->val + l2->val;
 
-			//resultList->val = sum;
-			list1 = list1->next;
-			list2 = list2->next;
+		while (list1->val && list2->val) {
+			sum = list1->val + list2->val + carryDigit;
+
+			currNode = new ListNode(sum % 10);
+
+			if (list1->next) {
+				list1 = list1->next;
+			}
+			else {
+				list1 = zeroNode;
+			}
+
+			if (list2->next) {
+				list2 = list2->next;
+			}
+			else {
+				list2 = zeroNode;
+			}
+
+			// First time through set Head Node
+			if (resultList->next == nullptr) {
+				resultList = currNode;
+				resultList->next = lastNode;
+			}
+			else {
+				lastNode->val = currNode->val;
+				lastNode->next = nextNode;
+				lastNode = lastNode->next;
+			}
 		}
 
+		std::cout << resultList->val << " ";
+		std::cout << resultList->next->val << " ";
+		std::cout << resultList->next->next->val << " ";
 		return resultList;
 	}
 };
@@ -56,17 +87,19 @@ int main() {
 	Solution solution;
 
 	// Test Case: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-	ListNode* l1n1 = &ListNode(2);
-	ListNode* l1n2 = &ListNode(4);
-	ListNode* l1n3 = &ListNode(3);
+	ListNode* l1n1 = new ListNode(2);
+	ListNode* l1n2 = new ListNode(4);
+	ListNode* l1n3 = new ListNode(3);
 	l1n1->next = l1n2;
 	l1n2->next = l1n3;
+	l1n3->next = NULL;
 
-	ListNode* l2n1 = &ListNode(5);
-	ListNode* l2n2 = &ListNode(6);
-	ListNode* l2n3 = &ListNode(4);
+	ListNode* l2n1 = new ListNode(5);
+	ListNode* l2n2 = new ListNode(6);
+	ListNode* l2n3 = new ListNode(4);
 	l2n1->next = l2n2;
 	l2n2->next = l2n3;
+	l2n3->next = NULL;
 
 	solution.addTwoNumbers(l1n1, l2n1);
 
