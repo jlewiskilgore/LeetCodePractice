a5eeb077e7a2e570293b752ad25235d441b950a2
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-08-19 12:29:18 -0400

    Continue Problem 4 - Cover Even total nums and empty vector cases

--- C++/Problem4_MedianTwoSortedArrays/MedianTwoSortedArrays.cpp
+++ C++/Problem4_MedianTwoSortedArrays/MedianTwoSortedArrays.cpp
@@ -21,33 +21,52 @@ public:
 		int isEven;
 		int medianIndex1;
 		int medianIndex2;
+		int searchCounter;
 		double medianValue;
+		double firstEvenMedianValue;
 		int nums1Index = 0;
 		int nums2Index = 0;
 
 		// Find index of median number(s)
 		if (totalNums % 2) {
 			isEven = 0;
-			medianIndex1 = (totalNums / 2) + 1;
+			medianIndex1 = (totalNums / 2) + 0.5;
+			searchCounter = medianIndex1;
 		}
 		else {
 			isEven = 1;
 			medianIndex1 = (totalNums / 2) - 1;
-			medianIndex2 = totalNums / 2;
+			medianIndex2 = (totalNums / 2);
+			searchCounter = medianIndex2;
 		}
+		
+		for (int i = 0; i <= searchCounter; i++) {
 
-		for (int i = 0; i < medianIndex1; i++) {
-			if ((nums2Index >= nums2.size()) || (nums1[nums1Index] < nums2[nums2Index])) {
+			if (isEven && i == searchCounter) {
+				firstEvenMedianValue = medianValue;
+			}
+
+			// Check if either array has been fully searched already
+			if (nums1Index >= nums1.size()) {
+				medianValue = nums2[nums2Index];
+				nums2Index++;
+			}
+			else if (nums2Index >= nums2.size()){
 				medianValue = nums1[nums1Index];
 				nums1Index++;
 			}
-			else if ((nums1Index >= nums1.size()) || (nums1[nums1Index] > nums2[nums2Index])) {
+			// Compare elements to find next "searched"
+			else if (nums1[nums1Index] < nums2[nums2Index]) {
+				medianValue = nums1[nums1Index];
+				nums1Index++;
+			}
+			else if (nums1[nums1Index] > nums2[nums2Index]) {
 				medianValue = nums2[nums2Index];
 				nums2Index++;
 			}
 		}
 
-		return medianValue;
+		return isEven ? (firstEvenMedianValue + medianValue) / 2.0 : medianValue;
 	}
 };
 
@@ -62,11 +81,52 @@ int main() {
 	 Expected Median: 2.0
 	 */
 	
-	std::vector<int> nums1 = { 1, 3 };
-	std::vector<int> nums2 = { 2 };
+	std::vector<int> test1_nums1 = { 1, 3 };
+	std::vector<int> test1_nums2 = { 2 };
 
-	double result = solution.findMedianSortedArrays(nums1, nums2);
-	std::cout << "Result: " << result << "\n";
+	double test1_result = solution.findMedianSortedArrays(test1_nums1, test1_nums2);
+	std::cout << "Result: " << test1_result << "\n";
+
+	/*
+	Test Case 2:
+	 nums1 = [1, 2]
+	 nums2 = [3, 4]
+	 Expected Median: 2.5
+	*/
+
+	std::vector<int> test2_nums1 = { 1, 2 };
+	std::vector<int> test2_nums2 = { 3, 4 };
+
+	double test2_result = solution.findMedianSortedArrays(test2_nums1, test2_nums2);
+	std::cout << "Result: " << test2_result << "\n";
+
+	/*
+	Test Case 3:
+	nums1 = []
+	nums2 = [2]
+	Expected Median: 2
+	*/
+
+	std::vector<int> test3_nums1 = { };
+	std::vector<int> test3_nums2 = { 2 };
+
+	double test3_result = solution.findMedianSortedArrays(test3_nums1, test3_nums2);
+	std::cout << "Result: " << test3_result << "\n";
+
+	/*
+	Test Case 4:
+	nums1 = [1]
+	nums2 = [1]
+	Expected Median: 1
+	*/
+
+	std::vector<int> test4_nums1 = { 1 };
+	std::vector<int> test4_nums2 = { 1 };
+
+	//double test4_result = solution.findMedianSortedArrays(test4_nums1, test4_nums2);
+	//std::cout << "Result: " << test4_result << "\n";
+
+
 
 	std::cout << "Press any key to exit...\n";
 	std::cin.get();
