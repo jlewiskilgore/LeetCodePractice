4e088a13db654138d02b71f677cf714ea07046e7
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-10-03 18:24:44 -0400

    Problem 22 - Finish Solution

--- C++/Problem22_GenerateParentheses/GenerateParentheses.cpp
+++ C++/Problem22_GenerateParentheses/GenerateParentheses.cpp
@@ -21,10 +21,39 @@ For example, given n =3, a solution set is:
 class Solution {
 public:
 	std::vector<std::string> generateParenthesis(int n) {
-		std::vector<std::string> resultCombinations = {"()", "()"};
+		std::vector<std::string> resultCombinations;
+
+		// Return empty vector
+		if (n == 0) {
+			return resultCombinations;
+		}
+
+		genParenHelper("", 0, 0, n, resultCombinations);
 
 		return resultCombinations;
-	};
+	}
+
+private:
+	void genParenHelper(std::string startStr, int numLeft, int numRight, int &n, std::vector<std::string> &resCombos) {
+		// Number left parentheses is greater than n, there are not enough right parentheses
+		if (numLeft > n) {
+			return;
+		}
+		// Valid String Found when parenthes counts are equal
+		if (numLeft == n && numRight == n) {
+			resCombos.push_back(startStr);
+		}
+		else {
+			// Consider case adding one more left parenthesis
+			genParenHelper(startStr+"(", numLeft+1, numRight, n, resCombos);
+			
+			// Consider case adding one more right parenthesis,
+			// if there are currently more left than right parenthesis currently
+			if (numLeft > numRight) {
+				genParenHelper(startStr+")", numLeft, numRight+1, n, resCombos);
+			}
+		}
+	}
 };
 
 int main() {
